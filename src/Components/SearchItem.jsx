9c46b21e1f44dd041928d5a1ec1186f098d192ac
Gautam Kumarm <g2gautamsingh@gmail.com>
2022-12-19 05:06:03 +0530

    done

--- src/Components/SearchItem.jsx
+++ src/Components/SearchItem.jsx
@@ -1,17 +1,16 @@
-
-import { Box, Flex, Image, Text } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SearchItem = ({ title, image, id }) => {
-    return (
-        <Link to={`/shoppingcart/${id}`}>
-            <Flex gap={15} marginTop={5} alignItems="center">
-                <Image width="50px" h="50px" src={image} alt={title} />
-                <Text >{title}</Text>
-            </Flex>
-        </Link>
-    )
-}
+  return (
+    <Link to={`/shoppingcart/${id}`}>
+      <Flex gap={15} marginTop={5} alignItems="center">
+        <Image width="50px" h="50px" src={image} alt={title} />
+        <Text>{title}</Text>
+      </Flex>
+    </Link>
+  );
+};
 
-export default SearchItem
+export default SearchItem;
